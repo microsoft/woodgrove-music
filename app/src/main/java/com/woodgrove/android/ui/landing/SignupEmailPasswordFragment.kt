2f9db5ffe1a14930da0cb4ee9bf667e05a8eb77e
SammyO <sodenhoven@microsoft.com>
2023-09-17 13:30:35 +0100

    sign up field validation + UI updates

--- app/src/main/java/com/woodgrove/android/ui/landing/SignupEmailPasswordFragment.kt
+++ app/src/main/java/com/woodgrove/android/ui/landing/SignupEmailPasswordFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.woodgrove.android.R
 import com.woodgrove.android.databinding.FragmentSignupEmailPasswordBinding
@@ -31,6 +32,51 @@ class SignupEmailPasswordFragment : Fragment() {
         binding.signupEmailPasswordNext.setOnClickListener {
             navigateNext()
         }
+        binding.signupEmailField.addTextChangedListener {
+            validateInputFields()
+        }
+        binding.signupPasswordField.addTextChangedListener {
+            validateInputFields()
+        }
+        binding.signupNameField.addTextChangedListener {
+            validateInputFields()
+        }
+        binding.signupEmailPasswordNext.setOnClickListener {
+            showLoading()
+            startSignup()
+        }
+    }
+
+    private fun validateInputFields() {
+        val emailValue = binding.signupEmailField.text
+        val passwordValue = binding.signupPasswordField.text
+        val nameValue = binding.signupNameField.text
+        if (!emailValue.isNullOrBlank() && !passwordValue.isNullOrBlank() && !nameValue.isNullOrBlank()) {
+            enableNextButton()
+        } else {
+            disableNextButton()
+        }
+    }
+
+    private fun enableNextButton() {
+        binding.signupEmailPasswordNext.isEnabled = true
+    }
+
+    private fun disableNextButton() {
+        binding.signupEmailPasswordNext.isEnabled = false
+    }
+
+    private fun showLoading() {
+        binding.signupEmailPasswordNext.text = null
+        binding.signupNextLoader.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+
+    }
+
+    private fun startSignup() {
+
     }
 
     private fun navigateNext() {
