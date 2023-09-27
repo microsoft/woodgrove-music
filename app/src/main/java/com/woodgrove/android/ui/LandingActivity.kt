303e6bd33438b5543e9848e49866a62306503f74
SammyO <sodenhoven@microsoft.com>
2023-09-27 12:01:27 +0100

    transparent background

--- app/src/main/java/com/woodgrove/android/ui/LandingActivity.kt
+++ app/src/main/java/com/woodgrove/android/ui/LandingActivity.kt
@@ -4,6 +4,9 @@ package com.woodgrove.android.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.woodgrove.android.R
 import com.woodgrove.android.databinding.ActivityLandingBinding
@@ -35,6 +38,9 @@ class LandingActivity : AppCompatActivity() {
     private fun initializeLandingListeners() {
         binding.landingSignup.setOnClickListener {
             openSignupOverlay()
+            Handler(Looper.getMainLooper()).postDelayed({
+                hideLogo()
+            }, 100)
         }
         binding.landingLogin.setOnClickListener {
             openLoginOverlay()
@@ -49,5 +55,10 @@ class LandingActivity : AppCompatActivity() {
     private fun openLoginOverlay() {
         startActivity(LoginActivity.getStartIntent(this))
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+    }
+
+    fun hideLogo() {
+        binding.landingMainText.visibility = View.GONE
+        binding.landingLogo.visibility = View.GONE
     }
 }
