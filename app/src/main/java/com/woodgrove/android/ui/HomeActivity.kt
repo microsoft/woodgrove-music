4b65d31fcad394ae68af17e4b211838e2351d624
yuxin <yuxin@microsoft.com>
2024-03-07 11:09:24 +0000

    revert changes on home

--- app/src/main/java/com/woodgrove/android/ui/HomeActivity.kt
+++ app/src/main/java/com/woodgrove/android/ui/HomeActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.woodgrove.android.databinding.ActivityHomeBinding
 
 class HomeActivity : Activity() {
